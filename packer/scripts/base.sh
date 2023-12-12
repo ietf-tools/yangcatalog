8f7ec3a3f49dcddf0c05111786d6bfd156650deb
Nicolas Giard <github@ngpixel.com>
2023-12-12 00:02:18 -0500

    ci: fix packer definition

--- packer/scripts/base.sh
+++ packer/scripts/base.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 export UCF_FORCE_CONFFOLD=1
+export DEBIAN_FRONTEND=noninteractive
 
-sudo apt-get -qy update
-sudo apt-get -qy -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' --force-yes upgrade
-sudo apt-get -qy -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' --force-yes install ca-certificates curl git gnupg openssl
-sudo install -m 0755 -d /etc/apt/keyrings
+apt-get -qy update
+apt-get -qy -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' --force-yes upgrade
+apt-get -qy -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' --force-yes install ca-certificates curl git gnupg openssl
+install -m 0755 -d /etc/apt/keyrings
