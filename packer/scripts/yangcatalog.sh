40094f2dd645d1fc44844a572fe75246c3f78a46
Nicolas Giard <github@ngpixel.com>
2023-12-14 00:33:45 -0500

    ci: fix packer definition

--- packer/scripts/yangcatalog.sh
+++ packer/scripts/yangcatalog.sh
@@ -25,7 +25,9 @@ cp /var/yang/yumapro-client-21.10-12.deb11.amd64.deb ./resources/yumapro-client-
 chmod 777 ./resources/confd-8.0.linux.x86_64.installer.bin
 
 # Temp Debug
-sed -i -e 's/npm ci/npm ci --loglevel verbose/g' /app/frontend/yangcatalog-ui/Dockerfile
+tar czvf /app/frontend/yangcatalog-ui/tmp/pt-topology-0.1.0.tgz --files-from=/dev/null
+
+#sed -i -e 's/npm ci/npm ci --loglevel verbose/g' /app/frontend/yangcatalog-ui/Dockerfile
 
 # Start containers
 docker compose build
